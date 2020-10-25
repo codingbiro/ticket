3a5c8729ad6367ad40e34d608a591f0fc465614f
codingbiro <hello@birovince.com>
2020-10-25 21:17:47 +0100

    user

--- middlewares/ticket/getTicketsMW.js
+++ middlewares/ticket/getTicketsMW.js
@@ -2,7 +2,7 @@ module.exports = function getTickets(objectrepository) {
   const { userModel } = objectrepository;
 
   return function getTicketsMW(req, res, next) {
-    userModel.find({ _id: req.session.user._id }, (err, user) => {
+    userModel.findOne({ _id: req.session.user._id }, (err, user) => {
       if (err) {
         req.session.sessionFlash = {
           type: 'danger',
