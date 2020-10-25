e877c32dde19d7ddb7ec41b2457b6cd90fce6461
codingbiro <hello@birovince.com>
2020-10-25 19:28:38 +0100

    asd

--- config/initDb.js
+++ config/initDb.js
@@ -1,10 +1,10 @@
 const UserModel = require('../models/user');
 
-export default function createAdminUser() {
+module.exports = function createAdminUser() {
   const newUser = new UserModel();
   newUser.name = 'Admin';
   newUser.email = 'admin@birovince.com';
   newUser.password = 'admin@birovince.com';
   newUser.isAdmin = true;
   newUser.save((err) => console.log(err));
-}
+};
