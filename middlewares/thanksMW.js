32998856e1e77361af9e84d8f9bdc19a5dfa6c7e
codingbiro <hello@birovince.com>
2020-10-27 21:50:51 +0100

    f

--- middlewares/thanksMW.js
+++ middlewares/thanksMW.js
@@ -2,12 +2,9 @@ module.exports = function thanks(objectrepository) {
   const { orderModel } = objectrepository;
 
   return function thanksMW(req, res, next) {
-    const { paymentId } = req.params;
-    console.log(paymentId);
-    console.log(orderModel);
+    const { paymentId } = req.query;
     orderModel.find({ pid: paymentId }, (err, order) => {
       if (err) {
-        console.log(err);
         return next(err);
       }
       console.log(order);
